647eebfb80e3ff70c90d17eef6541dcdd53e029b
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-02-10 20:49:44 +0100

    Non-golf hw3

---
+++ hw3/Golf.hs
@@ -0,0 +1,31 @@
+module Golf where
+
+-- 1.
+everyNth :: Int -> [a] -> [a]
+everyNth n xs = case drop (n -1) xs of
+  (y:ys) -> y : everyNth n ys
+  [] -> []
+
+skips :: [a] -> [[a]]
+skips xs = map (\n -> (everyNth n xs)) [1..(length xs)]
+
+-- 2.
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima (x:y:z:xs) 
+  | (y > z) && (y > x) = y : localMaxima (x:z:xs)
+  | otherwise = localMaxima (x:z:xs)
+localMaxima (_:_) = []
+
+-- 3.
+countN :: [Integer] -> Integer -> Int
+countN xs n = length (filter (\x -> x == n) xs)
+
+countAllInt :: [Integer] ->[Int]
+countAllInt xs = map (\n -> countN xs n) [0..9]
+
+produceStars :: [Int] ->[String]
+produceStars xs = map (\n -> (map (\x -> if x >= n then '*' else ' ') xs)) [1.. (maximum xs)]
+
+histogram :: [Integer] -> String
+histogram xs = unlines $ (reverse (produceStars (countAll xs))) ++ ["==========", "0123456789"]

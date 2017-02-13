fd3f04397441cd0cf6315384d15231eff4ac6c87
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-02-13 21:21:08 +0100

    Hw4-1 and ugly hw4-2

---
+++ hw4/hw4.hs
@@ -0,0 +1,34 @@
+module Hw4 where
+
+-- 1.
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n | even n = n + fun2 (n `div` 2)
+       | otherwise = fun2 (3 * n + 1)
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (\x -> x-2) . filter (even)
+
+fun2' :: Integer -> Integer
+fun2'  = sum . filter (even) . takeWhile (>1) . iterate (\x -> if (even x) then x `div` 2 else (x * 3 + 1)) 
+
+--  2.
+data Tree a = Leaf
+              | Node Integer (Tree a) a (Tree a) deriving (Show, Eq)
+
+foldTree :: [a] -> Tree a
+foldTree as = foldr insertNode Leaf as where 
+  insertNode :: a -> Tree a -> Tree a
+  insertNode a Leaf = Node 0 Leaf a Leaf
+  insertNode a (Node n Leaf val left) = Node (n + 1) (insertNode a Leaf) val left
+  insertNode a (Node n right val Leaf) = Node (n + 1) right val (insertNode a Leaf)
+  insertNode a (Node n right@(Node y _ _ _) val left@(Node y' _ _ _)) = if y < y' 
+                                                                          then Node (n + 1) (insertNode a right) val left
+                                                                          else Node (n + 1) right val (insertNode a left)

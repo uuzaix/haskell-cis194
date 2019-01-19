e4ed4634d11b15b98dae8c608bdbc6519d820758
Larisa Dolgova <larisa.dolgova@gmail.com>
2019-01-20 00:14:54 +0100

    hw2: Parse message

---
+++ 2018/hw2/LogAnalysis.hs
@@ -0,0 +1,15 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+import Log
+
+parseMessage :: String -> LogMessage
+-- parseMessage "E 2 562 help help" == LogMessage (Error 2) 562 "help help"
+-- parseMessage "I 29 la la la" == LogMessage Info 29 "la la la"
+-- parseMessage "This is not in the right format" == Unknown "This is not in the right format"
+
+parseMessage message = case (words message) of
+    "I":t:m -> LogMessage Info (read t) (unwords m)
+    "W":t:m -> LogMessage Warning (read t) (unwords m)
+    "E":s:t:m -> LogMessage (Error (read s) ) (read t) (unwords m)
+    _ -> Unknown message
+

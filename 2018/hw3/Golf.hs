b90d6fa17f2fd4c02541ddc5b164cbed6d863a68
Larisa Dolgova <larisa.dolgova@gmail.com>
2019-01-27 23:52:02 +0100

    Hw3-1,2

---
+++ 2018/hw3/Golf.hs
@@ -0,0 +1,33 @@
+module Golf where
+
+-- ex.1
+skips :: [a] -> [[a]]
+
+everyNth ::  [a] -> Int -> [a]
+everyNth xs n = case (drop (n - 1) xs ) of
+    (y:ys) -> y : (everyNth ys n)
+    [] -> []
+
+skips xs = map (everyNth xs) [1 .. length xs]
+
+-- skips "ABCD" == ["ABCD", "BD", "C", "D"]
+-- skips "hello!" == ["hello!", "el!", "l!", "l", "o", "!"]
+-- skips [1] == [[1]]
+    -- skips [True,False] == [[True,False], [False]]
+-- skips [] == []
+
+
+-- ex.2
+localMaxima :: [Integer] -> [Integer]
+localMaxima (x:y:z:xs)
+    | x < y && y > z = y : localMaxima(z:xs)
+    | otherwise = localMaxima(y:z:xs)
+localMaxima _ = []
+
+-- localMaxima [2,9,5,6,1] == [9,6]
+-- localMaxima [2,3,4,1,5] == [4]
+-- localMaxima [1,2,3,4,5] == []
+
+--ex.3
+-- histogram :: [Integer] -> String
+
